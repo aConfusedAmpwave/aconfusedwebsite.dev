798ee1ae00ac89561a4054916937e407987a4ac7
aConfusedAmpwave <aleiatgerhardt@gmail.com>
2024-05-21 13:10:24 -0700

    Fixed the Link issue

--- src/app/Header/Header.tsx
+++ src/app/Header/Header.tsx
@@ -1,10 +1,11 @@
 //This header will be default shown on the top of every page in the site, same with the footer at the bottom
 
 import { useState } from "react"
+import Link from "next/link"
 
 //Function to create links to the other pages on the site
 function PageLink({name, path}: {name: string, path: string}) {
-    return <h2><link href = {path}>{name}</link></h2>
+    return <h2><Link href = {path}>{name}</Link></h2>
 }
 
 export default function Header () {
@@ -19,14 +20,14 @@ export default function Header () {
     //Display the logo of the site in the left corner, followed by each page link (Home, Projects, About, Contact)
     return <>
         <header>
-            <link>
+            <Link href="/">
                 <img
                     src = "/public/Media Library/tempLogo.png"
                     alt = "Temporary Logo until logo completed"
                     width = {32}
                     height = {32}
                 />
-            </link>
+            </Link>
 
             <div>
                 <nav>
